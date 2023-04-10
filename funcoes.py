80523a379d3d60976dbd4b6748949e0f1119cf7c
viquecastro <victoriacastro3108@gmail.com>
2023-04-10 00:42:57 -0300

    Corrigindo erro de execução

--- funcoes.py
+++ funcoes.py
@@ -1,22 +1,22 @@
 #Essas são as funções que usaremos durante a execução do programa
 
-#Função que acessa o arquivo de instâncias e retorna uma lista com as linhas do arquivo
+#Função que acessa o arquivo de instâncias e retorna uma lista com as linhas do arquivo "cruas"
 def acessaArquivo(arq):
     meuArquivo= open(arq)
     listaInicial= meuArquivo.readlines()
     return listaInicial
 
-#Função que cria a lista de arestas do arquivo
+#Função que cria a lista de arestas do arquivo formatadas
 def listaArestas(listaInicial):
     novaLista=[]
     for i in range(4, len(listaInicial)):
         aux1=listaInicial[i]
-        aux2=aux1[0:3]
+        size=len(aux1)
+        aux2=aux1[:size -1]
         novaLista.append(aux2)
-        #novaLista.append(listaInicial[i])
     return novaLista
 
-#Função que retorna o número de vértices
+#Função que retorna o número de vértices totais do grafo
 def numVertices(listaInicial):
     vertices=listaInicial[2].split('=')
     numeroVerices=int(vertices[1])
@@ -27,18 +27,18 @@ def numVertices(listaInicial):
 def listaVizinhos(novaLista,x):
     arestasDeX=[]
     for i in novaLista:
-        if x==int(i[0]):
-            arestasDeX.append(int(i[2]))
-        elif x==int(i[2]):
-            arestasDeX.append(int(i[0]))
+        listaAux=i.split()
+        if x==int(listaAux[0]):
+            arestasDeX.append(int(listaAux[1]))
+        elif x==int(listaAux[1]):
+            arestasDeX.append(int(listaAux[0]))
     arestasDeX.sort()
     return arestasDeX
 
-#criando a lista de adjacências
-
+#Função que cria lista de adjacências usando dicionário
 def listaAdj(novaLista,numVertices):
     adjList= {}
-    adjList[0]=['avulso']
-    for i in range (1,numVertices):
+    adjList[0]=[0]
+    for i in range (1,numVertices+1):
         adjList[i]=listaVizinhos(novaLista,i)
     return adjList
--- main.py
+++ main.py
@@ -5,16 +5,17 @@ import funcoes as fun
 listArq=fun.acessaArquivo('0.in')
 listaArestas=fun.listaArestas(listArq)
 numvertices=fun.numVertices(listArq)
-vizinhosde4=fun.listaVizinhos(listaArestas,4)
+vizinhosde2=fun.listaVizinhos(listaArestas,2)
 
 #Temos a lista de arestas, agora precisamos percorrer essa lista caçando os vertices pra colocar na lista de adj
 
 #procura pelo vertice 1. funçao verificaaresta
 
-#print(listaArestas)
-#print(listaArestas[1][2])
-
+print(listArq)
 print(listaArestas)
+print(numvertices)
+print(vizinhosde2)
+
 
 listaAdjacencias=fun.listaAdj(listaArestas,numvertices)
 print(listaAdjacencias)
